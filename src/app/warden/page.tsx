d0e02cfbeecc2f9096dc02dd4d2db1cd1c822f9b
phoenixit99 <hoanguit@gmail.com>
2024-10-24 18:13:07 +0700

    Update page.tsx

--- src/app/warden/page.tsx
+++ src/app/warden/page.tsx
@@ -13,6 +13,8 @@ import {
   Tabs,
   TabList,
   Tab,
+  TabPanels,
+  TabPanel,
 } from "@chakra-ui/react";
 import { CheckCircleIcon } from "@chakra-ui/icons";
 import Header from "../components/Header";
@@ -48,7 +50,6 @@ const handleCardSelect = (id: number) => {
   return (
     <>
       <Header />
-      <WardenContent></WardenContent>
        <Flex width="100%">
         <Tabs
           variant="enclosed"
@@ -63,12 +64,35 @@ const handleCardSelect = (id: number) => {
               </Tab>
             ))}
           </TabList>
+          <TabPanels>
+            {menuItems.map((item, index) => (
+              <TabPanel key={item.id}>
+                {selectedItemIndex === index && (
+                  <ContentPanel selectedItem={item} />
+                )}
+              </TabPanel>
+            ))}
+          </TabPanels>
         </Tabs>
       </Flex> 
     </>
   );
 }
-
+const ContentPanel: React.FC<ContentPanelProps> = ({ selectedItem }) => {
+    return (
+    <Box p="20px">
+      <Text fontSize="3xl" fontWeight="bold">
+        {selectedItem.title}
+      </Text>
+      {selectedItem.id === 0 && <WardenContent />}
+      {selectedItem.id === 1 && <WardenContent />}
+      {selectedItem.id === 2 && <WardenContent />}
+      {selectedItem.id === 3 && <WardenContent />}
+      {selectedItem.id === 4 && <WardenContent />}
+      {selectedItem.id === 5 && <WardenContent />}
+    </Box>
+  );
+}
 const WardenContent = () => {
   return (
     <>
