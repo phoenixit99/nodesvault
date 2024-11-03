a9388a4844304550b5831cd4089c6df2f64e21b2
phoenixit99 <hoanguit@gmail.com>
2024-11-03 16:40:57 +0700

    update

--- src/app/warden/page.tsx
+++ src/app/warden/page.tsx
@@ -117,6 +117,130 @@ export default function Menu() {
     );
   };
 
+
+  const Command = () => {
+    const codeSnippets = [
+      {
+        title: "Wallet Commands:",
+        code: `
+            // Add New Wallet
+            wardend keys add wallet
+  
+            // Restore executing wallet
+            wardend keys add wallet --recover
+  
+            // List All Wallets
+            wardend keys list
+  
+            // Delete wallet
+            wardend keys delete wallet
+  
+            // Check Balance
+            wardend q bank balances $(wardend keys show wallet -a)
+  
+            // Show validator
+            wardend tendermint show-validator
+  
+            // Show EVM address
+            wardend keys unsafe-export-eth-key
+  
+            // Backup
+            Seed + priv_validator_key.json
+          `,
+      },
+      {
+        title: "Validator Commands:",
+        code: `
+            // Create Validator
+            wardend comet show-validator
+  
+            # Example validator info
+            {
+                "pubkey": {"@type":"/cosmos.crypto.secp256k1.PubKey","key":"A7JSLhhRRXl3/Cfyi1kDQXBsp8yA5Z2BOIPQQ6JzXy7K"},
+                "amount": "1000000uward",
+                "moniker": "Validator name",
+                "identity": "123123123-KEYBASE",
+                "website": "Link Website",
+                "security": "Contact(Email;X;Telegram)",
+                "details": "details validator",
+                "commission-rate": "0.1",
+                "commission-max-rate": "0.2",
+                "commission-max-change-rate": "0.01",
+                "min-self-delegation": "1"
+            }
+  
+            # Create the validator transaction
+            wardend tx staking create-validator $HOME/validator.json \
+            --from=wallet \
+            --chain-id=chiado_10010-1 \
+            --fees=500uward
+  
+            // Delegate your Validator
+            wardend tx staking delegate $(wardend keys show wallet --bech val -a) 1000000uward \
+                --from=wallet \
+                --chain-id=chiado_10010-1 \
+                --fees=500uward
+  
+            // Withdraw rewards and commission
+            wardend tx distribution withdraw-rewards $(wardend keys show wallet --bech val -a) \
+            --from wallet \
+            --commission \
+            --chain-id=chiado_10010-1 \
+            --fees=500uward
+  
+            // Unjail
+            wardend tx slashing unjail --from wallet --chain-id chiado_10010-1 --fees=500uward -y
+          `,
+      },
+      {
+        title: "Service Management Commands:",
+        code: `
+            # Reload Service
+            sudo systemctl daemon-reload
+  
+            # Enable Service
+            sudo systemctl enable wardend
+  
+            # Disable Service
+            sudo systemctl disable wardend
+  
+            # Start Service
+            sudo systemctl start wardend
+  
+            # Stop Service
+            sudo systemctl stop wardend
+  
+            # Restart Service
+            sudo systemctl restart wardend
+  
+            # Check Service Status
+            sudo systemctl status wardend
+  
+            # Check Service Logs
+            sudo journalctl -u wardend -f --no-hostname -o cat
+          `,
+      },
+    ];
+  
+    return (
+      <Flex direction="column" align="start" maxW="1200px" p={4} gap={6}>
+        <Heading size="lg">Command</Heading>
+  
+        {codeSnippets.map(({ title, code }, index) => {
+          const { hasCopied, onCopy } = useClipboard(code);
+          return (
+            <div key={index}>
+              <Text fontSize="1xl" fontWeight="bold">
+                {title}
+              </Text>
+              <CodeBox code={code} onCopy={onCopy} hasCopied={hasCopied} />
+            </div>
+          );
+        })}
+      </Flex>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -171,9 +295,7 @@ export default function Menu() {
           )}
 
           {selectedId === 4 && (
-            <p className="mt-4 text-center text-xl font-semibold text-gray-800">
-              Test
-            </p>
+            <Command />
           )}
 
           {selectedId === 5 && (
